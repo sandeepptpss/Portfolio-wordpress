aa2d846513a7ba15b7a4c4c5377681fb776e193d
officialtpss <officialtpss@gmail.com>
2025-06-09 19:06:53 +0530

    new update

--- wordpress/wp-content/themes/custom-theme/index.php
+++ wordpress/wp-content/themes/custom-theme/index.php
@@ -19,17 +19,14 @@
             <div class="row g-5">
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="d-flex align-items-center mb-5">
-                        <div class="years flex-shrink-0 text-center me-4">
-                            <h1 class="display-1 mb-0">5</h1>
-                            <h5 class="mb-0">Years</h5>
-                        </div>
-                        <h3 class="lh-base mb-0">of working experience as a web developer & designer</h3>
+                    <?php
+                    $skills_experience = get_field('skills_experience');?>
+                    <?php echo $skills_experience; ?>
                     </div>
                     <p class="mb-4">Bringing 5 years of hands-on experience in web design and development—delivering creative, user-focused, and results-driven solutions.</p>
                     <p class="mb-3"><i class="far fa-check-circle text-primary me-3"></i>Afordable Prices</p>
                     <p class="mb-3"><i class="far fa-check-circle text-primary me-3"></i>High Quality Product</p>
                     <p class="mb-3"><i class="far fa-check-circle text-primary me-3"></i>On Time Project Delivery</p>
-                    <a class="btn btn-primary py-3 px-5 mt-3" href="">Read More</a>
                 </div>
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="row g-3 mb-4">
